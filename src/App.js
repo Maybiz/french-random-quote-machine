28778b48b1b82e151a0c80bd474bddb04fe0141d
Maybiz <maxime.julliant@laposte.net>
2020-06-12 17:45:44 +0200

    ajout random color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import QuoteBox from './components/QuoteBox'
 
 const App = () => {
+
+   const [color, setColor] = useState('')
+
+   const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`
+
+   const style = {
+      background: randomColor
+   }
+
    return (
-      <div className="content">
+      <div className="content" style={style}>
          <QuoteBox />
       </div>
    )
--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,42 +1,49 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const QuoteBox = () => {
-   const quotes = [
-      {
-         text:
-            "C’est en vain qu’on cherche au loin le bonheur quand on oublie de le cultiver soi-même.",
-         author: "Jean-Jacques Rousseau",
-      },
-      {
-         text:
-            "Être heureux ne signifie pas que tout est parfait. Cela signifie que vous avez décidé de regarder au-delà des imperfections.",
-         author: "Aristote",
-      },
-      {
-         text:
-            "Le bonheur est un parfum que l’on ne peut répandre sur autrui, sans en faire rejaillir quelques gouttes sur soi-même.",
-         author: "Ralph Waldo Emerson",
-      },
-   ];
+	const quotes = [
+		{
+			text:
+				"C’est en vain qu’on cherche au loin le bonheur quand on oublie de le cultiver soi-même.",
+			author: "Jean-Jacques Rousseau",
+		},
+		{
+			text:
+				"Être heureux ne signifie pas que tout est parfait. Cela signifie que vous avez décidé de regarder au-delà des imperfections.",
+			author: "Aristote",
+		},
+		{
+			text:
+				"Le bonheur est un parfum que l’on ne peut répandre sur autrui, sans en faire rejaillir quelques gouttes sur soi-même.",
+			author: "Ralph Waldo Emerson",
+		},
+	];
 
-   const displayQuote = '';
+	const displayQuote = "";
 
-   const random = Math.floor(Math.random() * quotes.length)
+	const [index, setIndex] = useState(0);
 
-   const [index, setIndex] = useState(null)
+	const getRandom = () => {
+		setIndex(Math.floor(Math.random() * quotes.length));
+	};
 
-   useEffect(() => {
-      setIndex(random)
-   }, [])
+	useEffect(() => {
+		getRandom();
+	}, []);
 
-   return (
-      <div id="quote-box">
-         <div id="text"></div>
-         <div id="author"></div>
-         <a id="new-quote"></a>
-         <a id="tweet-quote"></a>
-      </div>
-   );
+	return (
+		<div id="quote-box">
+         <div className="top-row">
+            <div id="text">{quotes[index].text}</div>
+            <div id="author">{quotes[index].author}</div>
+         </div>
+			
+         <div className="bottom-row">
+            <button id="new-quote" onClick={getRandom}>Nouvelle citation</button>
+            <a id="tweet-quote"></a>
+         </div>        
+		</div>
+	);
 };
 
 export default QuoteBox;
